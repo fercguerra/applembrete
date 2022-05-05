77701e58c009a9db2705cce2d5f368b52f4127b6
Fernanda Cristina Guerra <60983422+fercguerra@users.noreply.github.com>
2022-05-04 23:22:14 -0300

    stateful

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,6 @@
 // Adicionar itens da lista
 // Remover itens da lista
 
-//Estrutura de Dados: conseguir ter algo para armezenar alguma coisa (entidade)
-//final: usado para valor imutável
-/* final list = <String>[];
-
-  void addLembrete(String value) {
-    list.add(value);
-  }
-
-  void removeLembrete(String value) {
-    list.remove(value);
-  }
-
-  addLembrete('EP 01');
-  removeLembrete('EP 01');
-  print(list);
-}
-*/
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,17 +10,47 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
-  
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+//Estrutura de Dados: conseguir ter algo para armezenar alguma coisa (entidade)
+  final list = <String>[];
+
+  void addLembrete(String value) {
+    list.add(value);
+    setState(() {});
+  }
+
+  void removeLembrete(String value) {
+    list.remove(value);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+      body: ListView(
+        children: list
+            .map((e) => ListTile(
+                  title: Text(
+                    e,
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  onTap: () {
+                    removeLembrete(e);
+                  },
+                ))
+            .toList(),
       ),
-      
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          addLembrete('EP 01');
+        },
+      ),
     );
   }
 }
